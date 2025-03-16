4bd41bf1693e98ce8a3cd252d6f5948f3c4d0516
Pancakes <patapancakes@pagefault.games>
2025-03-16 15:03:06 -0400

    Update copyright date in files

--- gozelle/file.go
+++ gozelle/file.go
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2024  Pancakes <patapancakes@pagefault.games>
+	Copyright (C) 2024-2025  Pancakes <patapancakes@pagefault.games>
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
